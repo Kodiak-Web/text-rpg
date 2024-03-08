063bfb81c6fd9fa7f750a9cc51fe6eb76da822f5
Charles LeGates <kodiakweb@proton.me>
2024-03-07 20:10:56 -0900

    more map

--- src/headers/map.hpp
+++ src/headers/map.hpp
@@ -1,30 +1,46 @@
 #pragma once
 #include <string>
 #include <vector>
+template <typename A>
+using VectorDict = std::vector<std::pair<std::string,A>>;
+typedef int chance;
+
 struct coords{
     int x;
     int y;
+    coords():x(0),y(0) {};
     coords(int x,int y) : x(x),y(y) {};
 
 };
+struct enemy{};
 
 class tile {
     public:
         std::string name;
-        std::vector<std::string,coords>;
-    private:
+        VectorDict<coords> NavigationDict;
+        coords coordinates;
+        std::string description;
+        enemy Enemy;
+
+        tile():coordinates() {}; 
+        tile(int x, int y): coordinates(x,y) {};
+
 };
 
 class map {
     private:
         std::vector<std::vector<tile>> maparray;
     public:
+        tile currentTile;
+        map() {};
+        
         tile get_tile(int x,int y) {
             if(maparray.size()-1<=x) {
                 if(maparray[x].size()-1<=y) {
                     return maparray[x][y];
                 }
             }
+            return currentTile;
         }
-
+ 
 };
