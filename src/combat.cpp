f2f04fdc56a11b1ac5ca11648497f87f5c6b87c0
Charles LeGates <kodiakweb@proton.me>
2024-03-14 15:54:39 -0800

    added verbose compile to CMakeLists for debugging and got some basic combat mechanics going to test.

--- src/combat.cpp
+++ src/combat.cpp
@@ -1,4 +1,6 @@
 #include "headers/combat.hpp"
+#include <iostream>
+#include <random>
 
 
 
@@ -33,4 +35,68 @@ void combatentity::damageStep(int damage, double DefenseModifier) {
 void combatentity::damageStep(int damage) {
         damageStep(damage,1.0);
     }
+attack::attack(int damageBase, int damageDieSize, int attackTime,int priority,std::string damageType):damageBase(damageBase),damageDieSize(damageDieSize),attackTime(attackTime) ,priority(priority), damageType(damageType) {}
+attack::attack() {
+    attack(1,0,128,128,"none");
+}
+int attack::rollDamage() {
+    return damageBase + rand()%(damageDieSize+1);
+}
 
+//returns 1 if fight win, 0 if fight loss.
+bool fightLoop(combatentity& player,combatentity& enemy, bool PlayerInitiative) {
+    bool PlayerTurn(PlayerInitiative);
+    bool attemptFlee(0);
+    while(true) {
+        if(PlayerTurn) {
+            int input;
+            std::cout << "1. Fight\n2.Flee" << std::endl;
+            std::cin >> input;
+            switch(input) {
+                case 1:
+                    playerAttackMenu(player,enemy);
+                    break;
+                case 2:
+                    attemptFlee = 1;
+                    break;
+            }
+        }else {
+            //enemy attack
+            PlayerTurn = true;
+        }
+    if (attemptFlee) {
+        return 1;
+        }
+    }
+    return 0;
+}
+
+void playerAttackMenu(combatentity player, combatentity enemy) {
+    std::string input;
+    for (const auto& [attackName, atkStruct] : player.attacks) {
+        std::cout << "- " << attackName << ": " << atkStruct.damageBase << 
+        '-' << atkStruct.damageBase + atkStruct.damageDieSize << " " << 
+        atkStruct.damageType << " damage" << std::endl;
+    }
+    std::cout << "Select attack by typing name.\n> ";
+    std::cin >> input;
+    try {
+        int damageRoll = (player.attacks[input].rollDamage());
+        bool critRoll = (rand()%20==0 ? true : false);
+        std::cout << damageRoll * (critRoll ? 2 : 1) << " damage" << std::endl;
+        enemy.damageStep(damageRoll,(critRoll ? 2 : 1));
+        std::cout << enemy.health << " HP" << std::endl;
+    }
+    catch(...) {
+        std::cout << "You don't know that attack" << std::endl;
+    }
+}
+void combatentity::add_attack(std::string attackName,attack Attack){
+    combatentity::attacks[attackName] = Attack; 
+}
+void combatentity::add_attack(std::pair<std::string,attack> attackElement) {
+    add_attack(attackElement.first,attackElement.second); 
+}
+void combatentity::add_attack(std::pair<std::string,attack>* attackElementPtr) {
+    add_attack(*attackElementPtr);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include "headers/map.hpp"
+#include "headers/globalstate.hpp"
+#include "headers/combat.hpp"
+#include <ctime>
+#include <iostream>
+#include <random>
+void init() {
+    srand(std::time(0));
+
+}
 
 
 int main() {
-
-
+    init();
+    attackregister Attacks = initAttacks();
+    combatantregister Combatants = initCombatants(Attacks);
+    tile debugroom(-256,-256);
+    debugroom.Enemy = Combatants["Dummy"]; 
+    fightLoop(Combatants["Player"],Combatants["Dummy"],true);
     return 0;
 }
