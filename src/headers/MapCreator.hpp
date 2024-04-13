75b7e031fb15c3498f667db8a42303b0fae07cec
Birdie907 <162518555+Birdie907@users.noreply.github.com>
2024-04-12 23:37:55 -0800

    Add files via upload
    Both the map object to make map building significantly easier and the player subclass to allow us to move the player token around easily

---
+++ src/headers/MapCreator.hpp
@@ -0,0 +1,90 @@
+/*A Prototype mapping system to host different objects within the game*/
+
+#include <iostream>
+#include <map>
+#include <unordered_set>
+
+using namespace std;
+
+//Represents an Object on a tile in the map
+class Object {
+public:
+	Object(int id) :id(id) {}
+
+	int getId() const {
+		return id;
+	}
+
+private:
+	int id;
+};
+
+//Class representing a tile on the map
+class Tile {
+public:
+	void addObject(const Object& object) {
+		objects.insert(object.getId());
+	}
+
+	void removeObject(const Object& object) {
+		objects.erase(object.getId());
+	}
+
+	bool hasObject(const Object& object) const {
+		return objects.find(object.getId()) != objects.end();
+	}
+
+private:
+	unordered_set<int> objects;
+};
+
+//Coordinates struct to represent coordinate positions of tiles
+struct Coordinates {
+	int x;
+	int y;
+
+	bool operator<(const Coordinates& other) const {
+		return x < other.x || (x == other.x && y < other.y);
+	}
+};
+
+//Worldmap class to represent the overall map behavior
+class Worldmap {
+public: 
+	Worldmap(int width, int height) : width(width), height(height) {
+		for (int y = 0; y < height;++y) {
+			for (int x = 0; x < width; ++x) {
+				tiles[{ x,y }] = Tile();
+			}
+		}
+	}
+
+	void addObject(const Coordinates& coords, const Object& object) {
+		if (isValidTile(coords)) {
+			tiles[coords].addObject(object);
+		}
+	}
+
+	void removeObject(const Coordinates& coords, const Object& object) {
+		if (isValidTile(coords)) {
+			tiles[coords].removeObject(object);
+		}
+	}
+
+	bool hasObject(const Coordinates& coords, const Object& object) const {
+		if (isValidTile(coords)) {
+			return tiles.at(coords).hasObject(object);
+		}
+		return false;
+	}
+
+	bool isValidTile(const Coordinates& coords) const {
+		return coords.x >= 0 && coords.x < width && coords.y >= 0 && coords.y < height;
+	}
+
+private:
+
+	int width;
+	int height;
+	std::map<Coordinates, Tile> tiles;
+};
---
+++ src/headers/Player.hpp
@@ -0,0 +1,35 @@
+/*Makes the Player object subclass with the ability to move within a space provided*/
+
+#include <iostream>
+#include "MapCreator.h"
+
+using namespace std;
+
+//Makes a working movable player object that is compatible with any created map
+class Player : public Object {
+public:
+	Player(int id, const Coordinates& initialCoords = { 0,0 }) : Object(id), coords(initialCoords) {}
+
+	Coordinates getCoordinates() const {
+		return coords;
+	}
+
+	void setCoordinates(const Coordinates& newCoords) {
+		coords = newCoords;
+	}
+
+	void move(Worldmap& worldmap, int dx, int dy) {
+		Coordinates newCoords = { coords.x + dx, coords.y + dy };
+		if (worldmap.isValidTile(newCoords)) {
+			worldmap.removeObject(coords, *this);
+			coords = newCoords;
+			worldmap.addObject(coords, *this);
+			setCoordinates(coords);
+		}
+		else
+			cout << "That, my dear Watson, would be hitting a wall." << endl;
+	}
+
+private:
+	Coordinates coords;
+};
